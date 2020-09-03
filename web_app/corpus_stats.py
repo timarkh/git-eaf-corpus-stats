be42a23e2705a94f605bc2ba63933d31103e032d
Timofey Arkhangelskiy <timarkh@gmail.com>
2020-09-03 20:19:29 +0200

    Added frequent tokens.

--- web_app/corpus_stats.py
+++ web_app/corpus_stats.py
@@ -3,6 +3,9 @@ import json
 import copy
 
 
+MAX_FREQ_TOKENS = 100
+
+
 class CorpusStats:
     """
     Contains methods for reading and processing basic statistics abot an ELAN corpus
@@ -29,12 +32,21 @@ class CorpusStats:
         totalDurSeconds = round(duration % 60)
         return str(totalDurHours).zfill(2) + ':' + str(totalDurMinutes).zfill(2) + ':' + str(totalDurSeconds).zfill(2)
 
+    def is_interviewer(self, speaker):
+        """
+        Based on speaker's code, determine if they are an interviewer.
+        """
+        if speaker.lower().startswith('interviewer'):
+            return True
+        return False
+
     def calculate_stats(self):
         """
         Calculate statistics for each corpus based on the data read previously.
         """
         for corpus in self.corpora:
-            corpus['speakers'] = set(sp for sp in corpus['dur_by_speaker']) & set(sp for sp in corpus['tok_by_speaker'])
+            corpus['speakers'] = list(set(sp for sp in corpus['dur_by_speaker']) & set(sp for sp in corpus['tok_by_speaker']))
+            corpus['informants'] = [sp for sp in corpus['speakers'] if not self.is_interviewer(sp)]
 
             corpus['total_sound_dur'] = 0
             if '#TOTAL_SOUND_DURATION' in corpus['dur_by_speaker']:
@@ -44,14 +56,15 @@ class CorpusStats:
 
             corpus['dur_by_speaker_str'] = {sp: self.str_duration(corpus['dur_by_speaker'][sp])
                                             for sp in corpus['dur_by_speaker']}
-            
+
             # Total transcribed duration for all speakers
             corpus['total_dur'] = sum(corpus['dur_by_speaker'][sp]
                                       for sp in corpus['dur_by_speaker'])
             # Total transcribed duration for informants
             corpus['inf_dur'] = sum(corpus['dur_by_speaker'][sp]
                                     for sp in corpus['dur_by_speaker']
-                                    if not sp.lower().startswith('interviewer'))
+                                    if not self.is_interviewer(sp))
+
             # Total token count for all speakers
             corpus['total_tok'] = 0
             corpus['total_tok_by_speaker'] = {}
@@ -60,13 +73,26 @@ class CorpusStats:
                 for tok in corpus['tok_by_speaker'][sp]:
                     corpus['total_tok'] += corpus['tok_by_speaker'][sp][tok]
                     corpus['total_tok_by_speaker'][sp] += corpus['tok_by_speaker'][sp][tok]
-            # Total token count for informants
+
+            # Total token count for informants only
             corpus['inf_tok'] = 0
             for sp in corpus['tok_by_speaker']:
-                if sp.lower().startswith('interviewer'):
+                if self.is_interviewer(sp):
                     continue
                 for tok in corpus['tok_by_speaker'][sp]:
                     corpus['inf_tok'] += corpus['tok_by_speaker'][sp][tok]
+
+            # Common frequency list of tokens
+            corpus['tok_freq'] = {}
+            for sp in corpus['tok_by_speaker']:
+                for token in corpus['tok_by_speaker'][sp]:
+                    if token not in corpus['tok_freq']:
+                        corpus['tok_freq'][token] = corpus['tok_by_speaker'][sp][token]
+                    else:
+                        corpus['tok_freq'][token] += corpus['tok_by_speaker'][sp][token]
+            corpus['freq_tokens'] = [token for token in sorted(corpus['tok_freq'],
+                                                               key=lambda t: (-corpus['tok_freq'][t], t))][:MAX_FREQ_TOKENS]
+            print(corpus['freq_tokens'])
 
             corpus['total_dur_str'] = self.str_duration(corpus['total_dur'])
             corpus['inf_dur_str'] = self.str_duration(corpus['inf_dur'])
